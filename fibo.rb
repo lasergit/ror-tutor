e377ca13977177c1d5cb870eab8fcb987603ec28
Siarhei Labyka <lasermail@list.ru>
2017-07-10 23:25:22 +0300

    fibo.rb: fixes hangs for 1 and 2 fib number request, protects from numbers different from natural.

--- fibo.rb
+++ fibo.rb
@@ -1,12 +1,20 @@
 #!/usr/bin/env ruby
 
 n = ARGV[0].to_i
+exit if n <= 0
 f = [0, 1]
 
 def fibo_next(f)
   f.push(f.last(2).inject(:+))
 end
 
-f = fibo_next(f) until f.size == n
+case n
+when 1
+  f.pop
+when 3..(1.0 / 0)
+  f = fibo_next(f) until f.size == n
+else
+  f = [] unless n == 2
+end
 
 puts f.join(' ')
